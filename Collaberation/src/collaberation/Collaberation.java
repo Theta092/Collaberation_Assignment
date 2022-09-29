d5f062684b9d57bc2ff415e466c43d749af9c073
Theta092 <nickmremp@icloud.com>
2022-09-29 10:29:44 -0500

    Commit

--- Collaberation/src/collaberation/Collaberation.java
+++ Collaberation/src/collaberation/Collaberation.java
@@ -8,7 +8,7 @@ public class Collaberation {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("akjscfie onsvouiwhe vf");
+		System.out.print("hi");
 		
 		System.out.println("hfhjhdfdkhdfkdjfhdfkjdshds kjsshfdskfhsfhskjfhfs dskjfdshfkjsfhs");
 	}

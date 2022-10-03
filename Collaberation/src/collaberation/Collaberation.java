831a3fce9bf33c653bd02b8118d2004a4d6d5e17
nickelz <nickelz@wcmp15vs019lt.blads.ca>
2022-10-03 10:17:46 -0500

    add numver

--- Collaberation/src/collaberation/Collaberation.java
+++ Collaberation/src/collaberation/Collaberation.java
@@ -10,11 +10,14 @@ public class Collaberation {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
+		int num;
 		System.out.println("hi");
 		System.out.println("I will ask you for a number.");
 		System.out.println("Pleas comply.");
 		System.out.print("\nPick a number: ");
+		num = input.nextInt();
 		System.out.println("You chose " + input.nextInt());
+		System.out.println("the number you chose was " + 1);
 	}
 
 }

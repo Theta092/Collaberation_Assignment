fa3dffefc4460e455f7459fe2eb3a353a576ab1f
nickelz <nickelz@wcmp15vs019lt.blads.ca>
2022-10-05 10:06:45 -0500

    just some changes

--- Collaberation/src/collaberation/LifeGUI.java
+++ Collaberation/src/collaberation/LifeGUI.java
@@ -4,18 +4,23 @@ import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Scanner;
 
 import javax.swing.*;
 
 public class LifeGUI {
+	Scanner input = new Scanner(System.in);
 	Life life = new Life(0, 0);
 	JFrame frame;
 	JPanel panel;
 	JLabel label;
 	JButton[][] cell;
 	String[][] a = new String[0][0];
+	int x, y;
+	JTextField quest;
 
 	public LifeGUI() {
+		
 		frame = new JFrame("Life");
 		frame.setSize(1000,1000);
 		frame.setLocationRelativeTo(null);
@@ -23,18 +28,24 @@ public class LifeGUI {
 		panel = new JPanel(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 		
+		label = new JLabel("what is the x value of the grid");
+		
 		for(int i=0; i<cell[0].length; i++) {
 			for(int j=0; j<cell[1].length; j++) {
 				cell[i][j] = new JButton();
 				cell[i][j].setActionCommand(a[i][j]);
-				cell[i][j]addActionListener(new ActionListener() {
+				cell[i][j].addActionListener(new ActionListener() {
 
 					@Override
 					public void actionPerformed(ActionEvent e) {				
 					}	
 				});
+				c.gridx = 1;
+				c.gridy = 4;
+				panel.add(label,c);
+				frame.setContentPane(panel);
+				frame.setVisible(true);
 			}
 		}
-		
 	}
 }
